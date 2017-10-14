5d02c4124a00b81802723715173ce9a6d977a56a
Marius Fagerland <mariusfagerland@yahoo.no>
2017-10-14 15:27:23 +0200

    Add allow access

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ mongoose.connect('mongodb://localhost/Basicdb');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 var routes = require('./api/routes/basic.server.routes');
 routes(app);
 
